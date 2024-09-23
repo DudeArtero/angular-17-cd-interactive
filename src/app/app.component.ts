9553f9dd57cc618119a8ce390f458b200f0964f0
DudeArtero <developersaj@gmail.com>
2024-09-23 18:02:53 +0200

    new project with 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, importProvidersFrom, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DefaultComponent } from './components/default-component/default.component';
 import { OnPushComponent } from './components/onpush-component/onpush.component';
+import { OrganizationChartModule } from 'primeng/organizationchart';
+import { TreeNode } from 'primeng/api';
+import { CommonModule } from '@angular/common';
 
 export enum ChangeDetectionType {
     DEFAULT = 'default',
@@ -17,17 +20,28 @@ export interface InteractiveComponent {
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, OnPushComponent, DefaultComponent],
+    imports: [CommonModule, RouterOutlet, OnPushComponent, DefaultComponent, OrganizationChartModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
 export class AppComponent {
-    appComponents: InteractiveComponent[] = [
+    appComponents: TreeNode<InteractiveComponent>[] = [
         {
-            type: ChangeDetectionType.DEFAULT,
-            title: "App component"
-        }
-    ]
+            data: {
+                type: ChangeDetectionType.DEFAULT,
+                title: 'App component',
+            },
+            children: [
+                {
+                    data: {
+                        type: ChangeDetectionType.DEFAULT,
+                        title: 'App component',
+                    },
+                    children: [],
+                },
+            ],
+        },
+    ];
 
     selectedComponent = signal<OnPushComponent | DefaultComponent | undefined>(undefined);
 
